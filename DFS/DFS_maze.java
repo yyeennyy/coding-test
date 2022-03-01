1c528f85705d29ead89e3a442e06691f67b81f01
Yeeun <69946286+yyhh314@users.noreply.github.com>
2022-03-01 15:33:46 +0900

    Update DFS_maze.java

--- DFS/DFS_maze.java
+++ DFS/DFS_maze.java
@@ -9,35 +9,41 @@ public class Main{
   public static boolean finishFlag = false;
   public static int[][] map = new int[200][200];
 
+  // 이동 방향 정의 (하우상좌)
+  // 우측하단을 먼저 고려하고자 한다
+  public static int[] dx = {0, 1, 0, -1};
+  public static int[] dy = {1, 0, -1, 0};
+
+  
   public static int DFS(int y, int x){
     // map[y][x]에 서 있어도 되는지 판단
     if (map[y][x] == 0) return 0;
     map[y][x] = 2;
 
-    int bellow = map[y+1][x];
-    int right  = map[y][x+1];
-    
     // 종료조건
     if (y == N && x == M) {
-      finishFlag = true;   
+      finishFlag = true;
+      map[y][x] = 2;
       return result++;
     }
 
     // 갈 곳이 있는 경우 재귀
-    // bellow와 right의 중복 처리 유의 => finishFlag 설정
-    if (bellow == 1) DFS(y+1, x);
-    if (finishFlag == true) return result++;
-    if (right == 1) DFS(y, x+1);
-    if (finishFlag == true) return result++;
+    // 미로를 찾았는데도 다른 i에서 이어서 길을 찾는 경우를 유의 
+    // => finishFlag 설정
+    for (int i=0; i<4; i++) {
+      int nx = x + dx[i]; 
+      int ny = y + dy[i];
+      if (map[ny][nx] == 1){
+        DFS(ny, nx);
+      } 
+      if (finishFlag == true) {
+        return result++;
+      }
+    }
     
     // 돌아가야 하는 경우 (재귀로 갱신된 맵 반영)
-    bellow = map[y+1][x];
-    right  = map[y][x+1];
-    if (bellow != 1 && right != 1){
-      map[y][x] = 3;
-      return 0;
-    }
-
+    // 위 for문을 지나 왔다면 자동으로 map[ny][nx] != 1
+    map[y][x] = 3;
     return 0;
   }
 
