fe1a6ca4a2ae2454f021a10d8a4585ec9c5f9440
yyeennyy <69946286+yyeennyy@users.noreply.github.com>
2023-08-13 17:16:48 +0900

    Create BOJ11729.java

---
+++ recursion/BOJ11729.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static int res = 0;
+    public static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        recur(1, 3, N);
+        System.out.println(res);
+        // System.out.println((int)Math.pow(2, N)-1);
+        System.out.println(sb);
+    }
+
+    public static void recur(int start, int target, int count) throws IOException{
+        if(count==1){
+            sb.append(String.format("%d %d\n", start, target));
+            res++;
+            return;
+        }
+        int mid = 6 - start - target;
+        recur(start, mid, count-1);
+        sb.append(String.format("%d %d\n", start, target)); 
+        res++;
+        recur(mid, target, count-1);
+    }
+}
