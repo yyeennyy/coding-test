3d3db6848c4c9f6e96d00369a1f7f5555b77ac3d
yyeennyy <69946286+yyeennyy@users.noreply.github.com>
2023-08-19 00:16:37 +0900

    Create BOJ15666_backtracing.java

---
+++ DFS/BOJ15666_backtracing.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+    public static int N, M;
+    public static int[] nums;
+    public static int[] arr;
+    public static StringBuilder sb = new StringBuilder();
+    public static Set<String> set = new HashSet<>();
+
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] line = br.readLine().split(" ");
+        N = Integer.parseInt(line[0]);
+        M = Integer.parseInt(line[1]);
+        line = br.readLine().split(" ");
+        nums = new int[N];
+        arr = new int[M];
+        for(int i=0; i<N; i++){
+            nums[i] = Integer.parseInt(line[i]);
+        }
+
+        Arrays.sort(nums);
+
+        backtracking(0, 0);
+        System.out.println(sb);
+    }
+
+    public static void backtracking(int i, int cnt){
+        if(cnt == M){
+            StringBuilder tmp = new StringBuilder();
+            for(int j=0; j<M; j++){
+                tmp.append(arr[j]);
+                tmp.append(" ");
+            }
+            if(set.contains(tmp.toString())) return;
+            set.add(tmp.toString());
+            sb.append(tmp.toString());
+            sb.append("\n");
+            return;
+        }
+        for(int j=i; j<N; j++){
+            arr[cnt] = nums[j];
+            backtracking(j, cnt + 1);
+        }
+    }
+}
