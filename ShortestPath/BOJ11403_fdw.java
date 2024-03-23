7f739967b5a27cecd5a87e6b24ed4851a0e8928e
yyeennyy <kye3314@gmail.com>
2024-03-23 10:29:09 +0900

    Algorithm: FloydWarshall - BOJ11403 - solution2

--- ShortestPath/BOJ11403_fdw.java
+++ ShortestPath/BOJ11403_fdw.java
@@ -7,38 +7,34 @@ public class Main {
 
     public static int N;
     public static int[][] map;
-    public static int[][] paths;
+    public static int inf = 100;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
         map = new int[N][N];
-        paths = new int[N][N];
 
         for (int i = 0; i < N; i++) {
             String[] nums = br.readLine().split(" ");
             for (int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(nums[j]);
+                // 아예 처음부터 map 에다가 반영해도 무관
+                // 가는 길이 없으면 inf 라는 것을 처음부터 map 에 반영
+                map[i][j] = Integer.parseInt(nums[j]) == 0 ? inf : 1;  // 아예 처음부터 map 에다가 반영해도 무관
             }
         }
 
         for (int n = 0; n < N; n++) {
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
-                    if (map[i][n] == 0 && map[n][j] == 0) continue;
-                    if ((map[i][n] != 0 && map[n][j] != 0)
-                            || (i == n && map[n][j] != 0)
-                            || (map[i][n] != 0 && n == j)) {
-                        paths[i][j] = 1;
+                    if (map[i][j] > map[i][n] + map[n][j])
                         map[i][j] = 1;
-                    }
                 }
             }
         }
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                System.out.print(paths[i][j] + " ");
+                System.out.printf("%d ", map[i][j] == inf ? 0 : 1);
             }
             System.out.println();
         }
